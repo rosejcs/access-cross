bdd81d9494d24c5f8f95f7a97c134fa10adb921e
jcs <rosejcs@163.com>
2019-08-23 01:21:27 +0800

    weather api get!

---
+++ src/views/Test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>123</h1>
+    <div>
+      <!-- <img src="http:" alt=""> -->
+      <!-- <img src="//i.tianqi.com/index.php?c=code&id=12&icon=1&num=5&site=12" /> -->
+      <!-- <iframe src="//i.tianqi.com/index.php?c=code&id=12&icon=1&num=5&site=12"></iframe> -->
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data(){
+    return {}
+  },
+  created(){
+    this.axios('https://www.tianqiapi.com/api/').then(res=>{
+      console.log(res);
+    })
+  }
+}
+</script>
+
+<style lang="" scope>
+  
+</style>
